37b695384096f6e1bd915536d0a3e012169688cc
cecharre <claudio.echarre@utec.edu.pe>
2021-08-10 16:45:14 -0500

    base springboot

---
+++ src/main/java/com/agri/quickchip/QuickchipApplication.java
@@ -0,0 +1,13 @@
+package com.agri.quickchip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickchipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickchipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/agri/quickchip/QuickchipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agri.quickchip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickchipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
