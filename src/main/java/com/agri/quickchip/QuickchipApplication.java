8fd15456ec6d736862d08d103714a06eae72a2a2
cecharre <claudio.echarre@utec.edu.pe>
2021-08-10 17:21:00 -0500

    add controller

--- src/main/java/com/agri/quickchip/QuickchipApplication.java
+++ src/main/java/com/agri/quickchip/QuickchipApplication.java
@@ -2,8 +2,10 @@ package com.agri.quickchip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"controller"})
 public class QuickchipApplication {
 
     public static void main(String[] args) {
